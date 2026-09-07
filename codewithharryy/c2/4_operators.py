f69f37ad4b32dca750bfa349a31a1b04c83c0cdf
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-07 12:00:47 +0530

    operators started

---
+++ codewithharryy/c2/4_operators.py
@@ -0,0 +1,23 @@
+a = 3
+b = 55
+
+# arithmetic operators
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# assignment operators(=, -=, +=, *=, /=)
+c= 4-2
+print(c)
+p=6
+p += 2
+print(p)
+
+# comparison operators(==, !=, >, <, >=, <=)
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
