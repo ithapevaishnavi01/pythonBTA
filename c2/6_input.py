0807a213cf0c7f47ad56f9b114f7dda8b84e35bb
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-08 21:43:44 +0530

    input

---
+++ c2/6_input.py
@@ -0,0 +1,10 @@
+a = input("enter no 1: ")
+
+b = input("enter no 2: ")   
+
+print("number a is ", a)
+print("number b is ", b)
+
+print ("sum" , int(a)+int(b))
+
+print ("sum" , a+b) # this will concatenate the two strings
