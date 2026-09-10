8ffd03ae7c2df8cd3c41e36bbd71edb12d5c0164
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-10 22:37:15 +0530

    string functions

---
+++ codewithharryy/c3/3_str_fun.py
@@ -0,0 +1,6 @@
+name = "vaishnavi"
+
+print(len(name))  #length of string
+print(name.endswith("vi"))  #check if string ends with "vi" , returns True or False
+print(name.startswith("v"))  #check if string starts with "v" , returns True or False
+print(name.capitalize())  #capitalizes the first letter of string
