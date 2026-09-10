eaacbd3290b766408779feec470ac4d792fe59fa
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-10 22:56:29 +0530

    prbs of c3

---
+++ codewithharryy/c3/prbbb1.py
@@ -0,0 +1,3 @@
+name = input("Enter your name: ")
+
+print(f"hello {name}!") # you can use this "+name+" also instead of f string but f string is more efficient and easy to use
---
+++ codewithharryy/c3/prbbb2.py
@@ -0,0 +1,7 @@
+letter = '''
+ hello name
+ yoyo
+ hey
+'''
+
+print(letter.replace("name","vaishnavi").replace("hey","bye"))  #replaces name with vaishnavi and hey with bye
