b83f131cbf5e69d2704356a0ae8dcd296a43afec
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-10 23:05:40 +0530

    done with c3 prbs

---
+++ codewithharryy/c3/prbbb3.py
@@ -0,0 +1,9 @@
+name = "hello my name is  vaishnavi  y"
+
+print(name.find("  "))  #finds the index of first occurrence of "  " in string 
+
+# if there is no occurrence of "  " in string it returns -1
+
+print(name.replace("  "," "))  #replaces "  " with " " in string
+
+print(name)  #prints the original string its dosent change the original string because strings are immutable in python
