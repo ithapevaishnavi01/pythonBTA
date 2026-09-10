e022df7904f7dbcc1800394fa539b6ac2c523f32
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-10 22:47:36 +0530

    done with escape sequance character

---
+++ codewithharryy/c3/4_escape.py
@@ -0,0 +1,14 @@
+# escape sequance character
+
+# \n   -> New line
+# \t   -> Horizontal tab
+# \\   -> Backslash
+# \'   -> Single quote
+# \"   -> Double quote
+# \r   -> Carriage return
+# \b   -> Backspace
+# \f   -> Form feed
+# \v   -> Vertical tab
+# \a   -> ASCII Bell / Alert
+# \0   -> Null character
+# \ooo -> Character with octal value
