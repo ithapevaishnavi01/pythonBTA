6da53af93f291fb2512254150325a0b5eff6c5d1
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-11 14:28:23 +0530

    started c5

---
+++ codewithharryy/c5/1_dict.py
@@ -0,0 +1,11 @@
+marks = {
+    "vaishnavi": 90,
+    "vinod": 80,
+    "Aditi": 70,
+    "list": [1, 2, 3, 4, 5],
+}
+
+print(marks , type(marks))  # prints the type of marks which is dict
+
+print(marks["vaishnavi"])  # prints the value of key "vaishnavi" which is 90
+
