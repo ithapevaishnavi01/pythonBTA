5b50f6ff919f75f41d5fe8e1dbf92843001d8d2e
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-11 14:05:42 +0530

    done with c4

---
+++ codewithharryy/c4/prbbb2.py
@@ -0,0 +1,13 @@
+marks=[]
+
+s1=int(input("Enter marks of student 1: "))
+marks.append(s1)  # adds s1 to the end of the list  
+s2=int(input("Enter marks of student 2: "))
+marks.append(s2)  # adds s2 to the end of the list
+s3=int(input("Enter marks of student 3: "))
+marks.append(s3)  # adds s3 to the end of the list
+marks.sort()  # sorts the list of marks
+print(marks)  # prints the sorted list of marks
+
+
+# int(input()) → takes integer input and the (inpput()) function takes string input
