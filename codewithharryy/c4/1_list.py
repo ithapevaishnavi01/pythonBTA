fc8646225a4d6a75378005892d40edb68756cd19
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-11 12:29:51 +0530

    started list

---
+++ codewithharryy/c4/1_list.py
@@ -0,0 +1,9 @@
+friends = ["vasihnavi" , "aditi" ,"ritesh" , 4 , 6]
+
+print(friends[0])
+
+friends[0] = "yoyo" # Change the first element of the list ,
+                    # in strings we cannot change the string but in
+                    #  list we can change the elements of the list because lists are mutable in python
+
+print(friends[0]) # Print the updated first element
