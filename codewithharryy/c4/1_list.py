417503a1a0d466a563ad0e65ef33a3a20174f979
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-11 12:32:28 +0530

    slicing in list

--- codewithharryy/c4/1_list.py
+++ codewithharryy/c4/1_list.py
@@ -7,3 +7,5 @@ friends[0] = "yoyo" # Change the first element of the list ,
                     #  list we can change the elements of the list because lists are mutable in python
 
 print(friends[0]) # Print the updated first element
+
+print(friends[2:5]) # Prints the elements from index 2 to 4 (5 is not included)
