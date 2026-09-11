9944b5224385b96a086df5ea78fe97e7f02ae1f6
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-11 13:11:06 +0530

    list methods

---
+++ codewithharryy/c4/2_list_method.py
@@ -0,0 +1,20 @@
+friends = ["vasihnavi" , "aditi" ,"ritesh" , 4 , 6]
+
+friends.append("harry") # Appends "harry" to the end of the list
+print(friends)
+
+l1=[1,2,3,9 , 3,]
+l1.sort() # Sorts the list in ascending order
+print(l1)
+
+# append() → adds item
+# extend() → adds multiple items
+# insert() → adds at position
+# remove() → removes value
+# pop() → removes by index
+# clear() → removes all
+# index() → finds position
+# count() → counts occurrences
+# sort() → sorts list
+# reverse() → reverses list
+# copy() → copies list
