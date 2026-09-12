70e1a6af49fe99958b68332fa5a48ec75e4444e3
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-12 14:27:02 +0530

    done with loops

---
+++ codewithharryy/c7/6_for_with_else.py
@@ -0,0 +1,7 @@
+l = [24,3564,758,866]
+
+for i in l :
+    print(i)
+
+else:
+    print("done") # this is print when the loop is exhausts , complete
---
+++ codewithharryy/c7/7_break_continue.py
@@ -0,0 +1,10 @@
+for i in range (100):
+    if (i == 34):
+        break #exit the loop right now , print nothing after 33
+    print(i)
+
+
+for j in range (100):
+    if (j == 34):
+        continue #skip the ineration , its skip the 24 and print the next value
+    print(j)
