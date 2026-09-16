89a388b163f8a26cf26ea14fb03cabf7ccd7fab2
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-16 10:44:44 +0530

    prb1 in c10

---
+++ codewithharryy/c10/5_prb1.py
@@ -0,0 +1,12 @@
+class programer:
+    comp="microsoft"
+    def __init__(self, name , salary,pincode):
+        self.name=name
+        self.salary=salary
+        self.pincode=pincode
+
+p=programer("harry",1200000,87698)
+print(p.name,p.salary,p.pincode)
+
+v=programer("vaishnavi",1200000,87698)
+print(v.name,v.salary,v.pincode)
