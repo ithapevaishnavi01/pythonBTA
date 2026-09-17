181eecc28bc3f61e19e4660a4b49fe1ad01fa7e5
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-17 22:22:21 +0530

    prb4 c11

--- codewithharryy/c11/10_prb3.py
+++ codewithharryy/c11/10_prb3.py
@@ -6,6 +6,8 @@ class emp:
     def Salaryafterinc(self):
         return (self.salary + self.salary * (self.increment / 100))
 
-
+    @increment.setter
+    def increment(self,salary):
+        
 e = emp()
 print(e.Salaryafterinc)
