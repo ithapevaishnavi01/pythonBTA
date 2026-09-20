af16e09066c95e85c9dd0ef74d733ba02f467a79
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-21 02:23:14 +0530

    datatype in java

--- java/Demo.java
+++ java/Demo.java
@@ -2,4 +2,4 @@ public class Demo{
     public static void main(String[] args){
         System.err.println("hello java");
     }
-}
+} 
---
+++ java/datatype.java
@@ -0,0 +1,20 @@
+public class datatype {
+    public static void main(String[] args) {
+        // byte b = 5;
+        byte b = 0b101;      //for  binary no we put 0b and then the binary number of that number
+                                // menas 5=101 for printing 5 we write 0b101        
+        short s = 20;
+        int i = 2000;
+        long l = 300000;
+
+        float f = 15.0f;
+        double d=56.659;
+
+        char c = 'A';
+
+        boolean bul = true ;
+
+
+        System.out.println(+b +"," +s +"," +i +"," +l +"," +f +"," +d +"," +c +"," +bul);
+    }
+}
