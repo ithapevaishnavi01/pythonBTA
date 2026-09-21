7a50fcebdbb08a66be45acc7532da73aa4044136
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-21 18:33:52 +0530

    datatype representation in binary,octal and hexadecimal

--- java/datatype.java
+++ java/datatype.java
@@ -1,8 +1,12 @@
 public class datatype {
     public static void main(String[] args) {
         // byte b = 5;
-        byte b = 0b101;      //for  binary no we put 0b and then the binary number of that number
-                                // menas 5=101 for printing 5 we write 0b101        
+        //byte b = 0b101;      //for  binary no we put 0b and then the binary number of that number
+                                // menas 5=101 for printing 5 we write 0b101    
+        
+        //byte b = 05;            //octal no representation
+        byte b = 0x5;            //hexadecimal representation
+                                
         short s = 20;
         int i = 2000;
         long l = 300000;
