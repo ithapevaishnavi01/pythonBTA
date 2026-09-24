d1f82f759d2118055c20403363cdebca66c0843e
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-24 23:17:08 +0530

    relational done

---
+++ java/day3_1.java
@@ -0,0 +1,13 @@
+public class day3_1 {
+    
+    public static void main(String[] args) {
+        //relational operators == , != , < ,> , <= , >= 
+        int a = 10;
+        int b = 10;
+        boolean c = (a == b);
+        System.out.println(c);
+
+        //check notes for all  
+        // output of all relational/comparsion operators comes in true or flase
+    }
+}
