dc9dfcd2099c701405e95b96d92b49456f3145eb
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-27 00:40:41 +0530

    while condition

---
+++ java/day6_1.java
@@ -0,0 +1,30 @@
+public class day6_1 {
+    public static void main(String[] args) {
+        //loops
+
+        /*
+ while loop
+            while(expression){
+            do something
+            }
+        
+        
+        */
+        int i =1;
+        // while (i <= 10) {
+        //     System.out.println(i);
+        //     i++;    
+        // }
+
+
+//do while loop
+
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+
+
+
+    }
+}
