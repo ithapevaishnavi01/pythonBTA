63dd5a80f52868bd88fe6a3c1a3cacf424adc3d0
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-26 23:10:14 +0530

    done selection statemant

---
+++ java/day5.java
@@ -0,0 +1,42 @@
+public class day5 {
+    public static void main(String[] args) {
+   
+    int i =12;
+
+    //selection statemnet
+            //normal if
+            // if(i == 4) {
+            //     System.out.println("its ture");
+            
+            // }
+            // System.out.println("done");
+            // }
+
+
+             //if-else 
+            //  if(i == 4) {
+            //     System.out.println("i is 5 ");
+            
+            // }
+            // else
+            //     {
+            //         System.out.println("i is not 5");
+            //     }
+ 
+            //nested if
+            if (i>5){
+                if(i<10){
+                    System.out.println("less than 10");
+                }
+                else{
+                    System.out.println("greater than 10");
+                }
+            }else{  
+                System.out.println("less than 5");
+
+            }
+
+
+
+         }
+}
