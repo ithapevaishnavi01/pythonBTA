85745b1b1648cd100c7d8da6354f18957bd38bb1
ithapevasihnavi01 <ithapevaishnavi01@gmail.com>
2026-09-26 14:07:45 +0530

    logical op

---
+++ day4_1.java
@@ -0,0 +1,13 @@
+public class day4_1 {
+   public static void main(String[] args){
+    
+      //logical op
+      int a = 5;
+      int b = 10;
+      int c = 15;
+
+      boolean d = (a<b)  && (b<c);
+          //  OR --> ||         //do from notes also short circuit
+      System.out.println(d);
+   }
+}
--- java/day4.java
+++ java/day4.java
@@ -10,7 +10,7 @@ public class day4 {
 
 
         System.out.println(+c + "," +d +"," +e +"," +f);
-
+        
 
         //shift op  
 
